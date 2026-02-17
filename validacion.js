45941e7d0e486fb9fd1319cf6b30a0d291761fb8
stephanie <222stephasantapiez@gmail.com>
2026-02-17 10:02:03 -0500

    validacion completa

--- validacion.js
+++ validacion.js
@@ -1,13 +1,25 @@
 let usuario = document.getElementById("usuario");
+let mensaje = document.getElementById("mensaje");
 
-usuario.addEventListener("input", function(e){
-this.value = this.value.replace(/[^a-zA-Z]/g, '');
-this.border = "2px solid"
-if(this.value == ""){
-    this.style.borderColor = "red"
-} else {
-    this.style.borderColor = "green"
-    this.nextElementSibling && (this.nextElementSibling.textContent = 'Nombre válido');
-    this.nextElementSibling && (this.nextElementSibling.style.color = 'green');
-}
+usuario.addEventListener("input", function () {
+
+    let valor = this.value;
+
+    if (/[^a-zA-Z]/.test(valor)) {
+        this.style.border = "2px solid red";
+        mensaje.textContent = "El texto que esta tratando de agregar no es valido";
+        mensaje.style.color = "red";
+    } 
+    else if (valor === "") {
+        this.style.border = "2px solid red";
+        mensaje.textContent = "Campo obligatorio";
+        mensaje.style.color = "red";
+    } 
+    else {
+        this.style.border = "2px solid green";
+        mensaje.textContent = "Usuario válido";
+        mensaje.style.color = "green";
+    }
+
+    this.value = valor.replace(/[^a-zA-Z]/g, '');
 });
